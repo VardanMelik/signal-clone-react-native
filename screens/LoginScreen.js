09f9309da2bb15aad8eb5e1299238d377f1beb65
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-11-10 18:30:14 +0400

    LoginPage

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,79 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, View, Text, KeyboardAvoidingView } from 'react-native'
 import { Button, Input, Image } from 'react-native-elements'
 
 const LoginScreen = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const signIn = () => {
+
+    }
+
     return (
-        <View>
+        <KeyboardAvoidingView 
+            behavior='padding'
+            /*enabled*/
+            style={styles.container}
+            >
             <StatusBar style="light" />
-            <Text>I am Login Screen</Text>
-        </View>
+            <Image 
+                source={{
+                    uri: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Signal-Logo.svg"
+                }} 
+                style={{ width: 200, height: 200}}
+            />
+            <View style={styles.inputContainer}>
+                <Input 
+                    placeholder="Email"
+                    type="email"
+                    autoFocus
+                    value={email}
+                    onChangeText={
+                        text => setEmail(text)
+                    }
+                />
+                <Input 
+                    placeholder="Password"
+                    type="password"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={
+                        text => setPassword(text)
+                    }
+                />
+            </View>
+            <Button 
+                title="Login"
+                onPress={signIn}
+                containerStyle={styles.button}
+            />
+            <Button 
+                title="Register"
+                type="outline"
+                containerStyle={styles.button}
+            />
+            <View style={{ height: 100 }} />
+        </KeyboardAvoidingView>
     )
 }
 
 export default LoginScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        backgroundColor: 'white'
+    },
+    inputContainer: {
+
+    },
+    button: {
+
+    }
+})
